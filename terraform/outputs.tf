ea5c47253ffb234594f575ad02b9c940ffe6817f
NikitaKotambe03 <kotambe.nikita.16ce7041@gmail.com>
2026-03-25 20:18:36 +0530

    Production ready terraform code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,8 +12,3 @@ output "private_subnet_id" {
   description = "Private Subnet ID"
   value       = aws_subnet.private_subnet.id
 }
-
-output "ec2_instance_id" {
-  description = "EC2 Instance ID"
-  value       = aws_instance.app_server.id
-}
